ea400aa6d47715cb9c7d0cb7bf1e8f97d6ad9530
Sergey Khoteev <khoteev.s@gmail.com>
2024-11-18 09:36:38 +0300

    Version 1. Follows coolant temp and generate signal for 3w valve

--- Boiler.ino
+++ Boiler.ino
@@ -1,110 +1,139 @@
 #include <GyverDS18.h>
+#include <GyverTM1637.h>
 #define SENSOR_PIN 3
-#define VALVE_OPEN_PIN 14
-#define VALVE_CLOSE_PIN 15
+#define POTENT_PIN A7
+#define VALVE_OPEN_PIN 9
+#define VALVE_CLOSE_PIN 10
+#define DISPLAY_TEMP_CLK 5
+#define DISPLAY_TEMP_NOW_DIO 6
+#define DISPLAY_TEMP_REQ_DIO 7
+#define coolant_temp_min 30
+#define coolant_temp_max 65
+#define close_relay 1
+#define open_relay 0
 GyverDS18Single sensor_coolant_temp(SENSOR_PIN);
+GyverTM1637 display_temp_now(DISPLAY_TEMP_CLK, DISPLAY_TEMP_NOW_DIO);
+GyverTM1637 display_temp_req(DISPLAY_TEMP_CLK, DISPLAY_TEMP_REQ_DIO);
 
 int coolant_temp_now = 0;
 int coolant_temp_difference = 0;
 int coolant_temp_difference_max = 0;
 int coolant_temp_required = 0;
+int coolant_temp_required_range = 0;
+
 bool temp_sensor_failure = false;
 
 bool valve_is_opening = false;
 bool valve_is_closing = false;
-unsigned long valve_acted_last_time = 0;
+unsigned long valve_started_action_last_time = 0;
 unsigned int valve_action_time_1000 = 1000;
+unsigned long valve_finished_action_last_time = 0;
+unsigned int valve_delay_time_1000 = 1000;
 
 unsigned int current_time = 0;
 
 bool test_mode = false;
 
-// ---------- TEMPERATURE MEASUREMENT SCRIPTS ----------
+// -------------------- COOLANT TEMPERATURE FUNCTIONS --------------------
 
-// UPDATE TEMP VALUE FROM THE SENSOR, IF FAIL RISE FLAG FOR SENSOL FAILURE BOOLEAN
-void coolant_get_temp() {
+// GETS TEMP VALUE FROM THE SENSOR AND UPDATES TEMP IN MEMORY, IF FAIL RISES FLAG FOR SENSOL FAILURE BOOLEAN
+void coolant_temp_now_update() {
   if (sensor_coolant_temp.ready()) {
     if (sensor_coolant_temp.readTemp()) {
       if (temp_sensor_failure == true) {
         temp_sensor_failure = false;
       }
       coolant_temp_now = sensor_coolant_temp.getTempInt();
+    } else {
+      temp_sensor_failure = true;
     }
-  } else {
-    temp_sensor_failure = true;
+    sensor_coolant_temp.requestTemp();
   }
 }
 
-// ---------- VALVE CONTROL SCRIPTS ---------- 
+// UPDATES IN MEMORY COOLANT TEMP REQUIRED RANGE
+int coolant_temp_return_required_range() {
+  return (((coolant_temp_max - coolant_temp_min) / 5) + 1) ;
+}
 
-// SEND SIGNAL TO START VALVE OPENING
+// UPDATES IN MEMORY COOLANT TEMP REQUIRED VALUE
+void coolant_temp_required_update() {
+  coolant_temp_required = coolant_temp_min + potentiometer_return_current_value() * 5;
+  coolant_temp_required = constrain(coolant_temp_required, coolant_temp_min, coolant_temp_max);
+}
+
+// -------------------- POTENTIOMETER FUNCTIONS --------------------
+
+// RETURNS STEP FOR POTENTIOMETER 
+int potentiometer_return_step_for_temp() {
+  return 1023 / coolant_temp_return_required_range();
+}
+
+// RETURNS VALUE ON POTENTIOMETER TAKING INTO ACCOUNT PRESELECTED STEP
+int potentiometer_return_current_value() {
+  return analogRead(POTENT_PIN) / potentiometer_return_step_for_temp();
+}
+
+// -------------------- VALVE CONTROL SCRIPTS -------------------- 
+
+// SENDS SIGNAL TO START VALVE OPENING
 void valve_opening_start_send_signal() {
-  digitalWrite(VALVE_OPEN_PIN, 1);
+  digitalWrite(VALVE_OPEN_PIN, open_relay);
 }
 
-// SEND SIGNAL TO STOP VALVE OPENING
+// SENDS SIGNAL TO STOP VALVE OPENING
 void valve_opening_finish_send_signal() {
-  digitalWrite(VALVE_OPEN_PIN, 0);
+  digitalWrite(VALVE_OPEN_PIN, close_relay);
 }
 
-// SEND SIGNAL TO START VALVE CLOSING
+// SENDS SIGNAL TO START VALVE CLOSING
 void valve_closing_start_send_signal() {
-  digitalWrite(VALVE_CLOSE_PIN, 1);
+  digitalWrite(VALVE_CLOSE_PIN, open_relay);
 }
 
-// SEND SIGNAL TO STOP VALVE CLOSING
+// SENDS SIGNAL TO STOP VALVE CLOSING
 void valve_closing_finish_send_signal() {
-  digitalWrite(VALVE_CLOSE_PIN, 1);
+  digitalWrite(VALVE_CLOSE_PIN, close_relay);
 }
 
 // SET OF ACTIONS FOR VAVLE OPENING START
 void valve_opening_start() {
+  Serial.println("Valve_opening_start");
   valve_is_opening = true;
-  valve_acted_last_time = current_time;
+  valve_started_action_last_time = current_time;
   valve_opening_start_send_signal();
-  if (test_mode == true) {
-    Serial.print("Valve_opening started at: ");
-    Serial.println(current_time);
-  }
 }
 
 // SET OF ACTIONS FOR VAVLE OPENING FINISH
 void valve_opening_finish() {
+  Serial.println("Valve_opening_finish");
   valve_is_opening = false;
+  valve_finished_action_last_time = current_time;
   valve_opening_finish_send_signal();
-  if (test_mode == true) {
-    Serial.print("Valve_opening finished at: ");
-    Serial.println(current_time);
-  }
 }
 
 // SET OF ACTIONS FOR VAVLE CLOSING START
 void valve_closing_start() {
+  Serial.println("Valve_closing_start");
   valve_is_closing = true;
-  valve_acted_last_time = current_time;
+  valve_started_action_last_time = current_time;
   valve_closing_start_send_signal();
-  if (test_mode == true) {
-    Serial.print("Valve_closing started at: ");
-    Serial.println(current_time);
-  }
 }
 
 // SET OF ACTIONS FOR VAVLE CLOSING FINISH
 void valve_closing_finish() {
+  Serial.println("Valve_closing_finish");
   valve_is_closing = false;
+  valve_finished_action_last_time = current_time;
   valve_closing_finish_send_signal();
-  if (test_mode == true) {
-    Serial.print("Valve_closing finished at: ");
-    Serial.println(current_time);
-  }
 }
 
 // RETURN OPERATION TIME, MS 
-int valve_action_time() {
-  return current_time - valve_acted_last_time;
+unsigned int valve_action_time() {
+  return current_time - valve_started_action_last_time;
 }
 
-// RETURN TRUE IF VALVE IN OPERATION MORE THAN PLANNED
+// RETURNS TRUE IF VALVE IN OPERATION MORE THAN PLANNED
 bool valve_is_operation_time_passed() {
   if (valve_action_time() >= valve_action_time_1000) {
     return true;
@@ -113,17 +142,35 @@ bool valve_is_operation_time_passed() {
   }
 }
 
-// VALVE STOP ALL KING OF OPERATION
-void valve_stop_operation() {
-  digitalWrite(VALVE_OPEN_PIN, 0);
-  digitalWrite(VALVE_CLOSE_PIN, 0);
+// RETURNS DELAY TIME, MS
+unsigned int vavle_delay_time() {
+  return current_time - valve_finished_action_last_time;
 }
 
-// ---------- TEMP ANALYSIS SCRIPTS ----------
+// RETURNS TRUE IF VALVE IN DELAY MORE THAN PLANNED
+bool valve_is_delay_time_passed() {
+  if (vavle_delay_time() >= valve_delay_time_1000) {
+    return true;
+  } else {
+    return false;
+  }
+}
 
-// UPDATE IN MEMORY DIFFERENCE BETWEEN REQUIRED TEMP AND CURRENT TEMP
+// VALVE STOPS ALL KING OF OPERATION
+void valve_stop_operation() {
+  if (valve_is_opening == true) {
+    valve_opening_finish();
+  } 
+  if (valve_is_closing == true) {
+    valve_closing_finish();
+  }
+}
+
+// -------------------- TEMP ANALYSIS SCRIPTS --------------------
+
+// UPDATES IN MEMORY DIFFERENCE BETWEEN REQUIRED TEMP AND CURRENT TEMP
 void coolant_get_temp_difference() {
-  coolant_temp_difference = coolant_temp_now - coolant_temp_required;
+  coolant_temp_difference = coolant_temp_required - coolant_temp_now;
 }
 
 // COMPARE CURRENT TEMP DIFF WITH DIFF MAX, MAKE DECISION IS TEMP CORRECTION NECESSITY, RETURN TRUE OF FALSE
@@ -144,7 +191,7 @@ bool valve_is_opening_required () {
   } 
 }
 
-// ---------- VALVE OPERATION SCRIPTS ----------
+// -------------------- VALVE OPERATION SCRIPTS --------------------
 
 // GENERAL DECISION FOR VALVE OPERATION
 void valve_general_control() {
@@ -167,7 +214,9 @@ void valve_start_operation() {
 // DECISION IF START OR FINISH FOR OPENING WILL BE IMPLEMENTED
 void valve_operation_opening() {
   if (valve_is_opening == false) {
-    valve_opening_start();
+    if (valve_is_delay_time_passed() == true) {
+      valve_opening_start();
+    }
   } else {
     if (valve_is_operation_time_passed() == true) {
       valve_opening_finish();
@@ -175,10 +224,12 @@ void valve_operation_opening() {
   }
 }
 
-// DECISION IF START OR FINISH CLOSING WILL BE IMPLEMENTED
+// DECISION IF START OR FINISH FOR CLOSING WILL BE IMPLEMENTED
 void valve_operation_closing() {
   if (valve_is_closing == false) {
-    valve_closing_start();
+    if (valve_is_delay_time_passed() == true) {
+      valve_closing_start();
+    }
   } else {
     if (valve_is_operation_time_passed() == true) {
       valve_closing_finish();
@@ -186,22 +237,79 @@ void valve_operation_closing() {
   }
 }
 
-  
+// -------------------- DISPLAY FUNCTIONS --------------------
 
-  
+// DYSPLAYS TEMP IN POS 0, 1
+void display_temperature(GyverTM1637 display, int temp) {
+  if (temp >= 10) {
+    display.display(0, temp / 10);
+    display.display(1, temp % 10);
+  }
+}
+
+// DISPLAYS _DEG _C IN POS 2, 3
+void display_celc_deg_(GyverTM1637 display) {
+  display.displayByte(2, _degree);
+  display.displayByte(3, _C);
+}
+
+
+// --- TEST FUNCTIOINS ---
+void test_relay_open() {
+  Serial.println("Opened");
+  digitalWrite(VALVE_OPEN_PIN, open_relay);
+  digitalWrite(VALVE_CLOSE_PIN, open_relay);  
+}
+
+void test_relay_close() {
+  Serial.println("Closed");
+  digitalWrite(VALVE_OPEN_PIN, close_relay);
+  digitalWrite(VALVE_CLOSE_PIN, close_relay);
+}
+
+void test_relay() {
+  test_relay_open();
+  delay(1000);
+  test_relay_close();
+  delay(1000);
+}
+
 
 void setup() {
+  // START SERIAL.
   Serial.begin(9600);
 
+  // TEMP SENSOR SETUP
   pinMode(SENSOR_PIN, INPUT);
+  sensor_coolant_temp.requestTemp();
+
+  // RELAY SETUP
   pinMode(VALVE_OPEN_PIN, OUTPUT);
   pinMode(VALVE_CLOSE_PIN, OUTPUT);
+  valve_stop_operation();
 
-  sensor_coolant_temp.requestTemp();
+  // DISPLAY SETUP
+  display_temp_now.clear();
+  display_temp_now.brightness(7);
+  display_celc_deg_(display_temp_now);
+  display_temp_req.clear();
+  display_temp_req.brightness(7);
+  display_celc_deg_(display_temp_req);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  coolant_get_temp();
+  current_time = millis();
+
+  // --- UPDATE TEMP VALUES ---
+  coolant_temp_now_update();
+  coolant_get_temp_difference();
+  coolant_temp_required_update();
+
+  // --- VALVE CONTROL ---
   valve_general_control();
+
+  // --- DISPLAY INFO ON DISPLAYS ---
+  display_temperature(display_temp_now, coolant_temp_now);
+  display_temperature(display_temp_req, coolant_temp_required);
+
 }
